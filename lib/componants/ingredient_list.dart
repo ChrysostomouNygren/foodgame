a9b39f22a62e27115f8afd12347610fb1dd0a4ba
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-02-17 11:07:32 +0100

    ingredient list cre8ed for oatmeal

---
+++ lib/componants/ingredient_list.dart
@@ -0,0 +1,7 @@
+import 'package:drag_drop/componants/ingredient.dart';
+
+const List<Ingredient> _oatmeal = [
+  Ingredient(ingredient: '2 dl Water'),
+  Ingredient(ingredient: '1 dl Oatmeal'),
+  Ingredient(ingredient: 'A pinch of salt')
+];
