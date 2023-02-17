47d845c410e3442f2418b57796b2696b55b2a981
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-02-17 09:33:53 +0100

    extracted star clipper componant

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,4 +1,5 @@
 import 'package:drag_drop/componants/cancel_btn.dart';
+import 'package:drag_drop/componants/star_clipper.dart';
 import 'package:drag_drop/pages/recipes_page.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
@@ -73,43 +74,6 @@ List<Item> _items = [
   //       NetworkImage('https://cdn-icons-png.flaticon.com/512/883/883760.png'),
   // ),
 ];
-
-class StarClipper extends CustomClipper<Path> {
-  StarClipper(this.numberOfPoints);
-
-  /// The number of points of the star
-  final int numberOfPoints;
-
-  @override
-  Path getClip(Size size) {
-    double width = size.width;
-    double halfWidth = width / 2;
-    double bigRadius = halfWidth;
-    double radius = halfWidth / 1.3;
-    double degreesPerStep = _degToRad(360 / numberOfPoints);
-    double halfDegreesPerStep = degreesPerStep / 2;
-    var path = Path();
-    double max = 2 * math.pi;
-    path.moveTo(width, halfWidth);
-
-    for (double step = 0; step < max; step += degreesPerStep) {
-      path.lineTo(halfWidth + bigRadius * math.cos(step),
-          halfWidth + bigRadius * math.sin(step));
-      path.lineTo(halfWidth + radius * math.cos(step + halfDegreesPerStep),
-          halfWidth + radius * math.sin(step + halfDegreesPerStep));
-    }
-    path.close();
-    return path;
-  }
-
-  double _degToRad(num deg) => deg * (math.pi / 180.0);
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    StarClipper oldie = oldClipper as StarClipper;
-    return numberOfPoints != oldie.numberOfPoints;
-  }
-}
 
 @immutable
 class ExampleDragAndDrop extends StatefulWidget {
