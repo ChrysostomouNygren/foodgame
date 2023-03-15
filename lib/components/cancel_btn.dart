e422a5a25f81dbcb7b513f3da65eac05278118e3
Katerina Chrysostomou Nygren <katerina.chrysostomou@hotmail.com>
2023-03-15 10:39:00 +0100

    Bug fixed, repopulate list on closing game

--- lib/components/cancel_btn.dart
+++ lib/components/cancel_btn.dart
@@ -1,20 +1,20 @@
-import 'package:drag_drop/components/cancel_modal.dart';
-import 'package:flutter/material.dart';
+// import 'package:drag_drop/components/cancel_modal.dart';
+// import 'package:flutter/material.dart';
 
-class CancelBtn extends StatelessWidget {
-  const CancelBtn({super.key});
+// class CancelBtn extends StatelessWidget {
+//   const CancelBtn({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        cancelModal(context);
-      },
-      child: SizedBox(
-        height: 40,
-        width: 40,
-        child: Image.asset('assets/close.png'),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextButton(
+//       onPressed: () {
+//         cancelModal(context);
+//       },
+//       child: SizedBox(
+//         height: 40,
+//         width: 40,
+//         child: Image.asset('assets/close.png'),
+//       ),
+//     );
+//   }
+// }
--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,6 +1,7 @@
 import 'package:drag_drop/components/cancel_btn.dart';
 import 'package:drag_drop/components/failed_modal.dart';
 import 'package:drag_drop/components/star_clipper.dart';
+import 'package:drag_drop/pages/home_page.dart';
 import 'package:drag_drop/pages/recipes_page.dart';
 import 'package:flutter/material.dart';
 import '../components/bin_tile.dart';
@@ -269,6 +270,117 @@ class _DragAndDropGameState extends State<DragAndDropGame>
     );
   }
 
+  Future<String?> cancelModal(context) {
+    return showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => Dialog(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  const SizedBox(
+                    height: 150,
+                    width: 200,
+                  ),
+                  Image.asset(
+                    'assets/stop.png',
+                    height: 120,
+                    fit: BoxFit.fill,
+                  )
+                ],
+              ),
+              const SizedBox(height: 15),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Center(
+                  child: Text(
+                    "Are you sure you want to cancel the game?",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        textStyle: const TextStyle(
+                          fontSize: 15,
+                        )),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/play_again.png',
+                          height: 50,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text('Resume'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        textStyle: const TextStyle(
+                          fontSize: 15,
+                        )),
+                    onPressed: () {
+                      setState(() {
+                        _items.clear();
+                        _items.addAll(_itemsConst);
+                        for (var btn in _sortingButtons) {
+                          btn.items.clear();
+                        }
+                      });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return const Home();
+                      }));
+                    },
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/cancel.png',
+                          height: 45,
+                          fit: BoxFit.fill,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Text('Cancel'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -285,10 +397,22 @@ class _DragAndDropGameState extends State<DragAndDropGame>
         ));
   }
 
+  Widget cancelBtn() {
+    return TextButton(
+        onPressed: () {
+          cancelModal(context);
+        },
+        child: SizedBox(
+          height: 40,
+          width: 40,
+          child: Image.asset('assets/close.png'),
+        ));
+  }
+
   PreferredSizeWidget _buildAppBar() {
     return AppBar(
       iconTheme: const IconThemeData(color: Color.fromARGB(166, 247, 247, 247)),
-      leading: const CancelBtn(),
+      leading: cancelBtn(),
       title: Text(
         'Pick the healthy food',
         style: Theme.of(context).textTheme.headlineMedium?.copyWith(
